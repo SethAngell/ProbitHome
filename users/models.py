4fd81e54701c80e6c8ad33bc415c709ee2c35a4d
SethAngell <sethangell.pl@gmail.com>
2019-07-11 13:56:35 -0400

    custom user model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+# users/models.py
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    pass
+    avatar = models.ImageField(upload_to='images/', default='default/DefaultProf.jpg')
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        super(CustomUser, self).save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.email
