ef9fcc598966032146f7d3c7709a209c9ee029ee
=SethAngell <SethAngell.PL@Gmail.com>
2019-08-16 21:50:08 -0400

    updating blog app to a class based view system

--- ProbitHome/settings.py
+++ ProbitHome/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/homepage/static/',
+    '/blog/static/'
 ]
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
